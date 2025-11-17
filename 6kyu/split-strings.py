339924970323a146afe286e0a8b988089476be75
t8fari <mjtafarii@gmail.com>
2025-11-17 23:17:23 +0000

    Split strings completed

---
+++ 6kyu/split-strings.py
@@ -0,0 +1,12 @@
+# Split Strings
+
+
+def solution(s):
+    """
+    Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+    Examples:
+    * 'abc' =>  ['ab', 'c_']
+    * 'abcdef' => ['ab', 'cd', 'ef']
+    """
+    return [s[i:i+2].ljust(2,'_') for i in range(0, len(s), 2)]
