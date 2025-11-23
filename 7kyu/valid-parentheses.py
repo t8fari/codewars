616e38f0523bc37e3573cdee6e9e8d0b216974c1
t8fari <mjtafarii@gmail.com>
2025-11-23 21:59:10 +0000

    Valid Parentheses completed

---
+++ 7kyu/valid-parentheses.py
@@ -0,0 +1,16 @@
+# Valid Parentheses
+
+
+def valid_parentheses(s):
+    """
+    Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+    Examples
+
+    "()"              =>  true
+    ")(()))"          =>  false
+    "("               =>  false
+    "(())((()())())"  =>  true
+    """
+    while '()' in s:
+        s = s.replace('()', '')
+    return not s
