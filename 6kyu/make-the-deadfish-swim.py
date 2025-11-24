c0085a51b33e16829fa707f60be9950ab4ad4e0b
t8fari <mjtafarii@gmail.com>
2025-11-24 22:59:28 +0000

    Make the deadfish swim completed

---
+++ 6kyu/make-the-deadfish-swim.py
@@ -0,0 +1,34 @@
+# Make the deadfish swim
+
+
+def parse(data):
+    """
+    Create a parser to interpret and execute the Deadfish language.
+
+    Deadfish operates on a single value in memory, which is initially set to 0.
+
+    It uses four single-character commands:
+
+        i: Increment the value
+        d: Decrement the value
+        s: Square the value
+        o: Output the value to a result array
+
+    All other instructions are no-ops and have no effect.
+    Examples
+
+    Program "iiisdoso" should return numbers [8, 64].
+    Program "iiisdosodddddiso" should return numbers [8, 64, 3600]
+    """
+    output = []
+    s = 0
+    for val in data:
+        if val == 'i':
+            s += 1
+        elif val == 'd':
+            s -= 1
+        elif val == 's':
+            s *= s
+        elif val == 'o':
+            output.append(s)
+    return output
