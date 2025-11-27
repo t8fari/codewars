c8825a4218eff396475e7e9e76b7c2f9d2b4439f
t8fari <mjtafarii@gmail.com>
2025-11-27 23:23:24 +0000

    Greed is good completed

---
+++ 5kyu/greed-is-good.py
@@ -0,0 +1,40 @@
+# Greed is Good
+
+
+def score(dice):
+    """
+    Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+    Three 1's => 1000 points
+    Three 6's =>  600 points
+    Three 5's =>  500 points
+    Three 4's =>  400 points
+    Three 3's =>  300 points
+    Three 2's =>  200 points
+    One   1   =>  100 points
+    One   5   =>   50 point
+
+    Each of 5 dice can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+    Example scoring
+
+    Throw       Score
+    ---------   ------------------
+    5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+    1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+    2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+
+    Note: your solution must not modify the input list.
+    """
+    threes = [6, 4, 3, 2]
+    score = 0
+    for face in set(dice):
+        if face in threes and dice.count(face)>=3:
+            score += (face*100)
+        elif face in (1,5):
+            x, y = divmod(dice.count(face), 3)
+            if face == 1:
+                score += (x*1000) + (y*100)
+            else:
+                score += (x*500) + (y*50)
+    return score
