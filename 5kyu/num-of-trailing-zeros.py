7ab64c1e927880da5d0e18c5ae9d62c5bf66c36b
t8fari <mjtafarii@gmail.com>
2025-11-27 23:23:52 +0000

    Number of trailing zeros in N! completed

---
+++ 5kyu/num-of-trailing-zeros.py
@@ -0,0 +1,30 @@
+# Number of trailing zeros of N!
+
+
+def zeros(n):
+    # how many numbers in range(n) have at least one 5 as a factor ==> 100/5 = 20
+    # how many of those numbers have at least two 5s (i.e. 25)as a factor ==> 20/5 = 4
+    # and so on.... until you have less than 5 numbers
+    # count = 20+4 = 24
+    count = 0
+    while n >= 5:
+        count += int(n / 5)
+        n = n / 5
+    return count
+
+
+def zeros(n):
+    count = 0
+    while n:
+        n = n // 5
+        count += n
+    return count
+
+
+def zeros(n):
+    i = 5
+    zeros = 0
+    while n >= i:
+        zeros += n // i
+        i *= 5
+    return zeros
