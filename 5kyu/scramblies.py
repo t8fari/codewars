cae9d38e3f1ebf8be5fb738d28bce51bdfffb5be
t8fari <mjtafarii@gmail.com>
2025-11-28 21:23:56 +0000

    Scramblies

---
+++ 5kyu/scramblies.py
@@ -0,0 +1,22 @@
+# Scramblies
+
+
+def scramble(s1, s2):
+    """
+    Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+    Notes:
+
+        Only lower case letters will be used (a-z). No punctuation or digits will be included.
+        Performance needs to be considered.
+
+    Examples
+
+    scramble('rkqodlw', 'world') ==> True
+    scramble('cedewaraaossoqqyt', 'codewars') ==> True
+    scramble('katas', 'steak') ==> False
+    """
+    for x in set(s2):
+        if s1.count(x) < s2.count(x):
+            return False
+    return True
