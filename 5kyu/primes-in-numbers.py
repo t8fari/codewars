2994c4d5ded313fdc6ea395b3ea62141b91048c2
t8fari <mjtafarii@gmail.com>
2025-11-28 21:57:51 +0000

    Fixed an issue where the function produced wrong answers for some numbers including 10

--- 5kyu/primes-in-numbers.py
+++ 5kyu/primes-in-numbers.py
@@ -1,15 +1,6 @@
 # Primes in numbers
 
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if not n%i:
-            return False
-    return True
-
-
 def prime_factors(n):
     """
     Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
@@ -20,29 +11,16 @@ def prime_factors(n):
 
     Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
     """
-    if is_prime(n):
-        return f"({n})"
-    
-    p_factors = [x for x in range(int(n**.5)+1) if is_prime(x)]
-    decomp = []
-    g = n
-    while not is_prime(g):
-        for f in p_factors:
-            if not g%f:
-                decomp.append(f)
-                g = g//f
-                break
-        if is_prime(g):
-            decomp.append(g)
-    return ''.join(f"({f}**{decomp.count(f)})" if decomp.count(f)>1 else f"({f})" for f in sorted(set(decomp)))
-
-
-
-# print(factors(8))
-# print(factors(86240))
-# print(prime_factors(86240))
-# print(prime_factors(120))
-# print(prime_factors(7775460))
-# print(factors(120)[1:-1])
-# print(is_prime(7919))
-print(prime_factors(25))
+    i = 2
+    decomp = ''
+    while n != 1:
+        count = 0
+        while not n%i:
+            n = n / i
+            count += 1
+        if count > 1:
+            decomp += f"({i}**{count})"
+        elif count == 1:
+            decomp += f"({i})"
+        i += 1
+    return decomp
