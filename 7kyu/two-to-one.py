14aa6a227f1107630603091be7dfb12bc9acb8d4
t8fari <mjtafarii@gmail.com>
2025-12-06 21:13:54 +0000

    Two to one completed

---
+++ 7kyu/two-to-one.py
@@ -0,0 +1,16 @@
+# Two to One
+
+
+def longest(a1, a2):
+    """
+    ake 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+    Examples:
+
+    a = "xyaabbbccccdefww"
+    b = "xxxxyyyyabklmopq"
+    longest(a, b) -> "abcdefklmopqwxy"
+
+    a = "abcdefghijklmnopqrstuvwxyz"
+    longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+    """
+    return ''.join(sorted(set(a1+a2)))
