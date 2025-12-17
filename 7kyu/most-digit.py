33678e888069adcdb65fefe84b4be2ef4fc64c7a
t8fari <mjtafarii@gmail.com>
2025-12-17 19:11:46 +0000

    Most Digit completed

---
+++ 7kyu/most-digit.py
@@ -0,0 +1,9 @@
+# Most Digits
+
+
+def find_longest(arr):
+    """
+    Find the number with the most digits
+    If two numbers in the argument array have the same number of digits, return the first one in the array
+    """
+    return max(arr, key=lambda x: len(str(x)))
